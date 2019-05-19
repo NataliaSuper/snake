bcef75a736d0191485b107a853b3611588b29b30
NataliaSuper <775438@mail.ru>
2019-05-19 15:21:12 +0300

    Добавила функцию Func1, как в репозитории Kartavetz, но не работает

--- Snake4/Snake4/Program.cs
+++ Snake4/Snake4/Program.cs
@@ -10,11 +10,16 @@ namespace Snake4
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            /* Point p1 = new Point(1, 3, '*');
+             p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+             Point p2 = new Point(4, 5, '#');
+             p2.Draw();*/
+
+            int x = 1;
+            Func1( x );
+            Console.WriteLine( "Call Func1.x = " + x );
+
 
             Console.ReadLine();
         }
